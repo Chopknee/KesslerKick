f1a64b32c885374613a1abe430e4293f0a294104
Chopknee <Chopknee@gmail.com>
2019-01-27 13:56:37 -0500

    Main menu globe randomizes rotation.

---
+++ Assets/Scripts/EarthRandomizer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRandomizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(
+            Random.RandomRange(0f, 359f),
+            Random.RandomRange(0f, 359f),
+            Random.RandomRange(0f, 359f)
+            );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
